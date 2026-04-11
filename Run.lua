43726f7133c669d334c10be9ee54c05bc7dec826
Dennis Hezel <dh@3yourmind.com>
2026-04-12 00:18:43 +0200

    Fix; do not show blizzard CM ui after leaving CM

--- Run.lua
+++ Run.lua
@@ -9,20 +9,21 @@ local g_currentRun = nil
 local g_currentInstanceId = 0
 local g_ticker = nil
 local g_watchFrameWasShown = nil
-local g_watchFrameScript = nil
 
 local function HideBlizzardChallengeModeTimer()
     if WatchFrame then
-        g_watchFrameScript = WatchFrame:GetScript("OnEvent")
-        g_watchFrameWasShown = WatchFrame:IsShown()
-        WatchFrame:SetScript("OnEvent", nil)
         WatchFrame:Hide()
+    end
+end
+
+local function StoreBlizzardChallengeModeTimer()
+    if WatchFrame then
+        g_watchFrameWasShown = WatchFrame:IsShown()
     end
 end
 
 local function RestoreBlizzardChallengeModeTimer()
     if WatchFrame then
-        WatchFrame:SetScript("OnEvent", g_watchFrameScript)
         if g_watchFrameWasShown then
             WatchFrame:Show()
         end
@@ -386,7 +387,6 @@ local function OnPlayerEnteringWorld(isInitialLogin, isReloadUI)
         else
             addon.RunUI:Hide()
         end
-        RestoreBlizzardChallengeModeTimer()
         return
     end
     HideBlizzardChallengeModeTimer()
@@ -404,6 +404,9 @@ local function OnPlayerLeavingWorld()
     if run then
         run.state.isStartTimeAccurate = false
         addon.RunHistory:UpdateCurrentRun(addon.Utility:DeepCopy(run))
+        RestoreBlizzardChallengeModeTimer()
+    else
+        StoreBlizzardChallengeModeTimer()
     end
 end
 
